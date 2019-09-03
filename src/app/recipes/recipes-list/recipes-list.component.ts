ffae718a68c6011e2b5b0f54992745ccc385b353
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2019-09-03 13:07:20 +0200

    adding Ingrediant and recipe model, working with recipe list and details vid:55

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe","A test description","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCFgzYhkty0oG9rdhJiw2Qh6Zx2PIpsGxlyNVjQSTx9FLev-_C"),
+    new Recipe("A Test Recipe","A test description","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCFgzYhkty0oG9rdhJiw2Qh6Zx2PIpsGxlyNVjQSTx9FLev-_C")
+  ];
 
   constructor() { }
 
